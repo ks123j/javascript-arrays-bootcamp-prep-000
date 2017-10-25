631a6663c1163b320b88026286ef2ceb0d688915
Katy <komal.vadukul@gmail.com>
2017-10-25 14:06:09 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
 
 function addElementToBeginningOfArray(array, element) {
@@ -38,4 +39,23 @@ function destructivelyRemoveElementFromEndOfArray(array) {
 
 function removeElementFromEndOfArray(array) {
   return array.slice(0,array.length-1);
+=======
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
+var array = [1];
+
+function addElementToBeginningOfArray(array, element) {
+  return array = ['element',...array];
+}
+
+function destructivelyAddElementToBeginningOfArray(array, element) {
+  return array = array.unshift('element');
+}
+
+function addElementToEndOfArray(array, element) {
+  return array = [...array, 'element'];
+}
+
+function destructivelyAddElementToEndOfArray(array, element) {
+  return array = array.push('element');
+>>>>>>> 6c809af2e0a25b94fadaabbf3cfd799d04359190
 }
